9634c870f95da56b6e40b2a3723c24ed5e9cd47f
Aybolek Hudayberdiyeva <ahudayberdiyeva@signifyhelth.com>
2024-03-17 07:33:49 -0500

    multiple beans same type

--- Spring-01-Core-IoC/src/main/java/com/aya/bean_annotation/AyaApp.java
+++ Spring-01-Core-IoC/src/main/java/com/aya/bean_annotation/AyaApp.java
@@ -10,8 +10,14 @@ public class AyaApp {
         ApplicationContext container=new AnnotationConfigApplicationContext(ConfigApp.class,ConfigAny.class);
 
        FullTimeMentor ft=container.getBean(FullTimeMentor.class);
-
        ft.createAccount();
+
+//       this will not work when we add 2 beans for the same class
+//       PartTimeMentor pt=container.getBean(PartTimeMentor.class);
+//       pt.createAccount();
+
+        PartTimeMentor pt=container.getBean("p2",PartTimeMentor.class);
+        pt.createAccount();
 
        String st=container.getBean(String.class);
        System.out.println(st);
--- Spring-01-Core-IoC/src/main/java/com/aya/bean_annotation/ConfigApp.java
+++ Spring-01-Core-IoC/src/main/java/com/aya/bean_annotation/ConfigApp.java
@@ -2,17 +2,42 @@ package com.aya.bean_annotation;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ConfigApp {
 
+
+ // when we have 2 beans for the same class we can default one of the by tagging @Primary
     @Bean
+    @Primary
     FullTimeMentor fullTimeMentor(){
+        System.out.println("Primary bean for FullTimeMentor");
         return  new FullTimeMentor();
     }
 
     @Bean
+    FullTimeMentor fullTimeMentor2(){
+        return  new FullTimeMentor();
+    }
+
+
+    @Bean(name = "p1")
     PartTimeMentor partTimeMentor(){
+        return new PartTimeMentor();
+    }
+
+
+//  Below method will cause an error, when we try to create 2 beans for the same class
+//No qualifying bean of type 'com.aya.bean_annotation.PartTimeMentor' available: expected single matching bean but found 2: partTimeMentor,partTimeMentor2
+//    @Bean
+//    PartTimeMentor partTimeMentor2(){
+//        return new PartTimeMentor();
+//    }
+
+    @Bean(name = "p2")
+    PartTimeMentor partTimeMentor2(){
+        System.out.println("P2 bean for PartTimeMentor");
         return new PartTimeMentor();
     }
 }
