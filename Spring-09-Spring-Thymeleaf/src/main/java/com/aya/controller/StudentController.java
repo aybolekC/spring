439c66ca72dbb35b55665e600e0e1dee4ca83bc6
Aybolek Hudayberdiyeva <ahudayberdiyeva@signifyhelth.com>
2024-03-24 02:35:07 -0500

    thymeleaf practice

---
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/aya/controller/StudentController.java
@@ -0,0 +1,30 @@
+package com.aya.controller;
+
+
+import com.aya.bootstrap.DataGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+
+    @RequestMapping("/register")
+    public String register(Model model){
+
+        model.addAttribute("students", DataGenerator.createStudent());
+        return "/student/register";
+    }
+
+
+    @RequestMapping("/welcome")
+    public String info(Model model){
+
+        model.addAttribute("students", DataGenerator.createStudent());
+        return "/student/welcome";
+    }
+
+
+}
