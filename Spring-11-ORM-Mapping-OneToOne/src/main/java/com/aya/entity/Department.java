788a857c9e61202a8588b1ce0be9c64e20759735
Aybolek Hudayberdiyeva <ahudayberdiyeva@signifyhelth.com>
2024-04-10 09:29:24 -0500

    oneToOne mapping

---
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/aya/entity/Department.java
@@ -0,0 +1,28 @@
+package com.aya.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "departments")
+@Data
+@NoArgsConstructor
+public class Department extends BaseEntity {
+
+
+    private String department;
+    private String division;
+
+    @OneToOne(mappedBy = "department")
+    private Employee employee;
+
+    public Department(String department, String division) {
+        this.department = department;
+        this.division = division;
+    }
+}
---
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/aya/entity/Employee.java
@@ -0,0 +1,42 @@
+package com.aya.entity;
+
+
+import com.aya.enums.Gender;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employees")
+@Data
+@NoArgsConstructor
+public class Employee extends BaseEntity {
+
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate hireDate;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    private int salary;
+
+    @OneToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+
+    public Employee(String firstName, String lastName, String email, LocalDate hireDate, Gender gender, int salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.hireDate = hireDate;
+        this.gender = gender;
+        this.salary = salary;
+    }
+}
