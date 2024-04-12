ecbdd7173bac5b2344f564a2cc88c874d3bfba5b
Aybolek Hudayberdiyeva <ahudayberdiyeva@signifyhelth.com>
2024-04-12 15:52:37 -0500

    Cinema mini project - flyway data migration

---
+++ Spring-13-ORM-CinemaLab/src/main/java/com/aya/entity/Account.java
@@ -0,0 +1,27 @@
+package com.aya.entity;
+import com.aya.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "account_details")
+public class Account extends BaseEntity{
+
+    private String name;
+    private String address;
+    private String country;
+    private String state;
+    private String city;
+    private Integer age;
+    private String postalCode;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role=UserRole.USER;
+
+    @OneToOne(mappedBy = "account")
+    private UserAccount user;
+
+}
